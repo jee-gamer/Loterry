dc64994908b18e478f405795588d4c90d7c13ae1
Jaykayy <jiramate2548@gmail.com>
2023-04-17 11:32:55 +0700

    Tried to create get_users_vote function doesn't work yet

--- Database/app.py
+++ Database/app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from werkzeug.serving import run_simple
 
 # DB Stuff
-from Database.database import Base, User
+from Database.database import Base, User, Bet
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -13,6 +13,7 @@ app = Flask(__name__)
 
 engine = create_engine('sqlite:////tmp/test.db', echo=False)
 Base.metadata.create_all(engine)
+
 
 @app.route("/users")
 def get_users():
@@ -26,6 +27,21 @@ def get_users():
     output = ""
     for u in session.query(User):
         output += f"\t{u.firstName}\t{u.lastName.strip()}\t\t{u.enabled}\n"
+    return f"<p>{output}</p>"
+
+
+@app.route("/users")
+def get_users_vote():
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    user = Bet(11, 999, 1)
+    session.add(user)
+    user = Bet(12, 999, 4)
+    session.add(user)
+    session.commit()
+    output = ""
+    for b in session.query(Bet):
+        output += f"\t{b.idUser}\t{b.idLottery}\t\t{b.userBet}\n"
     return f"<p>{output}</p>"
 
 
