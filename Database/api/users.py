f6507c791b63548244428c6682b5014a388ab012
Jaykayy <jiramate2548@gmail.com>
2023-04-28 18:16:36 +0700

    make post_user work flawlessly (I think) added get and post user_vote

--- Database/api/users.py
+++ Database/api/users.py
@@ -7,20 +7,55 @@ import json
 from hashlib import sha1
 import logging
 import datetime
+from sqlalchemy import func
 
-
+#  ahah broken database path
 logging.basicConfig(format='%(asctime)s %(message)s',
                     datefmt='%m/%d/%Y %I:%M:%S %p',
                     level=logging.INFO)
 
 
-def get_users(id) -> dict:
-    if id > 11:
-        return jsonify({'error': 'User doesnt exit'}), 400
-    return jsonify([u.alias for u in session.query(User)])
+def get_user(id) -> dict:
+    user = session.query(User).filter(User.idUser == id).first()
+    if user:
+        return jsonify(user.as_dict())
+    return jsonify(f"user not found")
 
 
-def post_users() -> dict:
+def post_user() -> dict:
     data = request.get_json()
     print(data)
+
+    user = session.query(User).filter(User.idUser == data["id"]).first()
+    if user:
+        return {'message': 'User with same id already exist'}
+
+    user = User(data["id"], data["alias"], data["firstName"], data["lastName"])
+    session.add(user)
+    session.commit()
+    return {'message': 'data received'}
+
+
+def get_user_vote(id):
+    bet = session.query(Bet).filter(Bet.idUser == id).all()
+    if not bet:
+        return {'message': 'User vote not found'}
+    return jsonify([v.as_dict() for v in session.query(Bet).filter(User.idUser == id).all()])
+
+
+def post_user_vote() -> dict:
+    data = request.get_json()
+    print(data)
+
+    sameBet = session.query(Bet).filter(Bet.idUser == data["idUser"], Bet.idLottery == data["idLottery"], Bet.userBet == data["userBet"]).all()
+    if sameBet:
+        return {'message': 'This bet is a Duplicate'}
+    maxId = session.query(func.max(Bet.idBet)).scalar()
+    if not maxId:
+        maxId = 0
+    idBet = maxId + 1
+
+    bet = Bet(idBet, data["idUser"], data["idLottery"], data["userBet"])
+    session.add(bet)
+    session.commit()
     return {'message': 'data received'}
